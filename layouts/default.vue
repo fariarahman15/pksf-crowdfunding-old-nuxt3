a8cbb693b3f74baa319be4339e42573f5fbb616c
fariarahman15 <fariarahman.dev@gmail.com>
2023-07-19 18:26:06 +0600

    default layout changed

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,26 +14,7 @@ const {
 </script>
 <template>
   <body class="hold-transition sidebar-mini layout-fixed">
-    <div class="wrapper">
-      <!-- Preloader -->
-      <common-preloader :preloader="preloader"></common-preloader>
-      <!-- Navbar -->
-      <common-navbar></common-navbar>
-      <!-- /.navbar -->
-      <!-- Main Sidebar Container -->
-      <common-header></common-header>
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-      <slot />
-      </div>
-      <!-- /.content-wrapper -->
-      <common-footer></common-footer>
-      <!-- Control Sidebar -->
-      <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-      </aside>
-      <!-- /.control-sidebar -->
-    </div>
+    <slot />
   </body>
 </template>
 
---
+++ pages/index_old.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <body class="text-3xl font-bold underline text-red-800">
+    <h1>Laravel 10 Nuxt 3 Role Permissions</h1>
+  </body>
+</template>
+
+<style scoped></style>
