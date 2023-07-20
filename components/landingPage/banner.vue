f7e9e970925119ac87c2e9c297edd85a18a53761
fariarahman15 <fariarahman.dev@gmail.com>
2023-07-20 18:09:37 +0600

    landing page design

---
+++ components/landingPage/banner.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="bg-[#0D5169] px-6 py-16">
+      <div
+        class="flex-col flex lg:flex-row gap-10 items-center justify-between max-h-[400px]"
+      >
+        <div class="w-[40%] p-5">
+          <h1 class="text-5xl text-white font-semibold mb-3">
+            Exploring Innovative Funding Ideas
+          </h1>
+          <div class="flex">
+            <div class="form-control rounded-full p-0">
+              <input
+                type="text"
+                placeholder="Search"
+                class="input input-bordered w-24 md:w-auto rounded-full"
+              />
+            </div>
+            <button class="-m-7 p-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div class="w-[50%] p-5">
+          <img
+            src="https://images.pexels.com/photos/2407634/pexels-photo-2407634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            class="w-full rounded-lg shadow-2xl"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/landingPage/navbar.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="navbar bg-[#DEF4FF]">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li><a>Item 1</a></li>
+            <li>
+              <a>Parent</a>
+              <ul class="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+              </ul>
+            </li>
+            <li><a>Item 3</a></li>
+          </ul>
+        </div>
+        <a class="normal-case text-xl cursor-pointer"
+          ><img class="w-[250px]" src="~/public/images/logo_pksf.png" alt=""
+        /></a>
+      </div>
+
+      <div class="w-[30%] flex justify-end lg:justify-between gap-5">
+        <div class="hidden lg:flex items-center">
+          <a class="link link-hover p-2">About us</a>
+          <a class="link link-hover p-2">How it works</a>
+          <a class="link link-hover p-2">Projects</a>
+
+          <div class="p-3">
+            <a href="" class="btn btn-sm btn-success rounded-full px-3 py-1"
+              >Join</a
+            >
+          </div>
+        </div>
+
+        <div>
+          <a href="" class="link link-hover p-2">Login</a>
+          <div class="dropdown dropdown-end">
+            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+              <div class="w-10 rounded-full">
+                <img
+                  src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                />
+              </div>
+            </label>
+            <ul
+              tabindex="0"
+              class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a class="justify-between">
+                  Profile
+                  <span class="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
