c020bdd8b986cf6e1c6755bf5c46d2c643ce4763
fariarahman15 <fariarahman.dev@gmail.com>
2023-07-23 16:00:06 +0600

    new banner section

---
+++ components/landingPage/bannerTwo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="px-16 py-12">
+    <div class="bg-[#DEF4FF] px-6 py-9 rounded-[20px] border">
+      <div
+        class="flex-col flex lg:flex-row gap-10 items-center justify-between max-h-[400px]"
+      >
+        <div class="w-[50%] px-9">
+          <h1 class="text-4xl text-[#034E6F] font-semibold mb-3">
+            Exploring Innovative Funding Ideas
+          </h1>
+          <p class="text-[24px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            ab odio labore accusamus repellat et quaerat dolorum beatae ipsa
+            nostrum aperiam deserunt doloremque, suscipit sint magnam. Quam
+            nulla officia omnis nihil veritatis eaque vitae deleniti. Mollitia
+            libero sit unde totam fugiat corrupti ipsum repellat nostrum, aut
+            veniam impedit tenetur quas!
+          </p>
+        </div>
+        <div class="w-[50%] p-2">
+          <iframe
+            class="rounded-md w-full h-[350px]"
+            src="https://www.youtube.com/embed/-yhtVSW65Ag"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
   <div>
     <LandingPageNavbar />
     <LandingPageBanner />
+    <LandingPageBannerTwo />
   </div>
 </template>
