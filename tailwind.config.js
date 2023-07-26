57241a812e95ab2c8b371d9d326736fa9dca28a3
fariarahman15 <fariarahman.dev@gmail.com>
2023-07-26 17:25:21 +0600

    customized color palette and screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,24 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "430px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+
+      colors: {
+        black: "#000000",
+        white: "#FFFFFF",
+        base: "#F3FBFF",
+        primary: "#0D5169",
+        secondary: "#DEF4FF",
+        accent: "#119CCA",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
