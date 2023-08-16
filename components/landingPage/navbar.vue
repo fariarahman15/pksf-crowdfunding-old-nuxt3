657a32f37e6fbb52de9031fce1ca0dbfe7e9aa34
fariarahman15 <fariarahman.dev@gmail.com>
2023-08-16 12:28:38 +0600

    navbar dropdown fixed

--- components/landingPage/navbar.vue
+++ components/landingPage/navbar.vue
@@ -8,15 +8,98 @@
           </label>
           <ul
             tabindex="0"
-            class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box"
           >
-            <li><a>About us</a></li>
             <li>
-              <a>How it works</a>
+              <details>
+                <summary class="text-primary font-bold">About us</summary>
+                <ul>
+                  <li><a> About FundwithPKSF</a></li>
+                  <li><a>About PKSF</a></li>
+                  <li>
+                    <a>RMTP </a>
+                  </li>
+                  <li><a>Partner Organizations (POs) </a></li>
+                  <li><a>Success stories </a></li>
+                  <li><a>FAQs </a></li>
+                  <li><a>Podcasts </a></li>
+                  <li><a>Contact us</a></li>
+                </ul>
+              </details>
             </li>
-            <li><a>Projects</a></li>
+
+            <li>
+              <details>
+                <summary class="text-primary font-bold">How it works</summary>
+                <ul>
+                  <li><a>How FundwithPKSF will work? </a></li>
+                  <li><a>What is Crowdfunding? </a></li>
+                  <li>
+                    <a>How Microfinance helps? </a>
+                  </li>
+                  <li><a>How Grants work? </a></li>
+                  <li><a>How Debt financing work? </a></li>
+                  <li>
+                    <a>How Equity investment work? </a>
+                  </li>
+                  <li>
+                    <a>Potential Enterprises of RMTP </a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+
+            <li>
+              <details>
+                <summary class="text-primary font-bold">Projects</summary>
+                <ul>
+                  <li>
+                    <a class="text-accent font-bold">Sub-projects</a>
+                    <ul>
+                      <li>
+                        <details>
+                          <summary class="font-bold">Horticulture</summary>
+
+                          <ul>
+                            <li><a>Safe Vegetables </a></li>
+                            <li><a>High Value Flower Expansion </a></li>
+                            <li><a>High Value Fruits </a></li>
+                            <li><a>Ecological Farming </a></li>
+                            <li><a>Safe Edible Oil </a></li>
+                            <li><a>Integrated Agriculture</a></li>
+                          </ul>
+                        </details>
+                      </li>
+                      <li>
+                        <details>
+                          <summary class="font-bold">Livestock</summary>
+
+                          <ul>
+                            <li><a>Safe Poultry </a></li>
+                            <li><a>Safe Meat and Dairy</a></li>
+                          </ul>
+                        </details>
+                      </li>
+                      <li>
+                        <details>
+                          <summary class="font-bold">
+                            Safe Meat and Dairy
+                          </summary>
+
+                          <ul>
+                            <li><a>Safe Fish </a></li>
+                            <li><a> Safe Street Food </a></li>
+                          </ul>
+                        </details>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </details>
+            </li>
           </ul>
         </div>
+
         <a class="normal-case text-xl cursor-pointer"
           ><img class="w-[250px]" src="~/public/images/logo_pksf.png" alt=""
         /></a>
@@ -24,9 +107,138 @@
 
       <div class="w-[50%] flex justify-end lg:justify-between gap-5">
         <div class="hidden lg:flex items-center space-x-10">
-          <a class="link link-hover p-2">About us</a>
+          <!-- <a class="link link-hover p-2">About us</a>
           <a class="link link-hover p-2">How it works</a>
-          <a class="link link-hover p-2">Projects</a>
+          <a class="link link-hover p-2">Projects</a> -->
+
+          <!-- <ul class="menu menu-horizontal px-1">
+            <li tabindex="0">
+              <details>
+                <summary>About us</summary>
+                <ul class="p-2">
+                  <li><a>About FundwithPKSF</a></li>
+                  <li><a>About PKSF</a></li>
+                  <li>
+                    <a>Rural Microenterprise Transformation Project (RMTP) </a>
+                  </li>
+                  <li><a>Partner Organizations (POs) </a></li>
+                  <li><a>Success stories </a></li>
+                  <li><a>FAQs </a></li>
+                  <li><a>Podcasts </a></li>
+                  <li><a>Contact us</a></li>
+                </ul>
+              </details>
+            </li>
+            <li tabindex="0">
+              <details>
+                <summary>How it works</summary>
+                <ul class="p-2">
+                  <li><a>a. How FundwithPKSF will work? </a></li>
+                  <li><a>b. What is Crowdfunding? </a></li>
+                  <li>
+                    <a>c. How Microfinance helps? </a>
+                  </li>
+                  <li><a>d. How Grants work? </a></li>
+                  <li><a>e. How Debt financing work? </a></li>
+                  <li>
+                    <a>f. How Equity investment work? </a>
+                  </li>
+                  <li>
+                    <a> g. Potential Enterprises of RMTP </a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li tabindex="0">
+              <details>
+                <summary>Projects</summary>
+                <ul class="p-2">
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
+                </ul>
+              </details>
+            </li>
+          </ul> -->
+
+          <div class="dropdown">
+            <label tabindex="0" class="whitespace-nowrap">About us </label>
+            <ul
+              tabindex="0"
+              class="menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box"
+            >
+              <li><a> About FundwithPKSF</a></li>
+              <li><a>About PKSF</a></li>
+              <li>
+                <a>Rural Microenterprise Transformation Project (RMTP) </a>
+              </li>
+              <li><a>Partner Organizations (POs) </a></li>
+              <li><a>Success stories </a></li>
+              <li><a>FAQs </a></li>
+              <li><a>Podcasts </a></li>
+              <li><a>Contact us</a></li>
+            </ul>
+          </div>
+
+          <div class="dropdown">
+            <label tabindex="1" class="whitespace-nowrap">How it works </label>
+            <ul
+              tabindex="1"
+              class="menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box"
+            >
+              <li><a>How FundwithPKSF will work? </a></li>
+              <li><a>What is Crowdfunding? </a></li>
+              <li>
+                <a>How Microfinance helps? </a>
+              </li>
+              <li><a>How Grants work? </a></li>
+              <li><a>How Debt financing work? </a></li>
+              <li>
+                <a>How Equity investment work? </a>
+              </li>
+              <li>
+                <a>Potential Enterprises of RMTP </a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="dropdown">
+            <label tabindex="3" class="whitespace-nowrap">Projects </label>
+            <ul
+              tabindex="3"
+              class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box"
+            >
+              <li>
+                <a class="text-accent font-bold">Sub-projects</a>
+                <ul>
+                  <li>
+                    <a class="font-bold">Horticulture </a>
+                    <ul>
+                      <li><a>Safe Vegetables </a></li>
+                      <li><a>High Value Flower Expansion </a></li>
+                      <li><a>High Value Fruits </a></li>
+                      <li><a>Ecological Farming </a></li>
+                      <li><a>Safe Edible Oil </a></li>
+                      <li><a>Integrated Agriculture</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a class="font-bold">Livestock </a>
+                    <ul>
+                      <li><a>Safe Poultry </a></li>
+                      <li><a>Safe Meat and Dairy</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a class="font-bold">Safe Meat and Dairy </a>
+                    <ul>
+                      <li><a>Safe Fish </a></li>
+                      <li><a> Safe Street Food </a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
 
           <div class="p-3">
             <a href="" class="btn btn-sm btn-success rounded-full px-3 py-1"
