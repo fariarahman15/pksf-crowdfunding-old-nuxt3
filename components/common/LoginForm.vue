b7b0bc7b9f145e75062615ae7ee7b8838168bee4
fariarahman15 <fariarahman.dev@gmail.com>
2023-08-21 12:49:26 +0600

    login and registration page

---
+++ components/common/LoginForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <form class="w-full space-y-3">
+    <div class="border-b border-teal-500 py-2">
+      <input
+        class="appearance-none bg-transparent w-full text-gray-900 mr-3 px-2 leading-tight focus:outline-none"
+        type="text"
+        placeholder="Username"
+        aria-label="Username"
+      />
+    </div>
+
+    <div class="flex items-baseline border-b border-teal-500 py-2">
+      <input
+        class="appearance-none bg-transparent w-full text-gray-900 mr-3 px-2 leading-tight focus:outline-none"
+        type="password"
+        placeholder="Password"
+        aria-label="Password"
+      />
+      <label class="swap">
+        <input type="checkbox" />
+        <div class="swap-on"><i class="fa-solid fa-eye"></i></div>
+        <div class="swap-off">
+          <i class="fa-solid fa-eye-slash"></i>
+        </div>
+      </label>
+    </div>
+    <a
+      href="#"
+      class="flex justify-end text-xs text-gray-600 hover:underline hover:text-blue-600"
+      >Forget Password?</a
+    >
+
+    <div class="mb-6">
+      <label
+        class="md:w-2/3 block text-gray-500 font-bold md:flex md:items-center"
+      >
+        <input class="mr-2 leading-tight" type="checkbox" />
+        <span class="text-sm"> Remember me </span>
+      </label>
+    </div>
+
+    <div>
+      <button
+        class="mt-6 w-full bg-accent hover:bg-primary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Sign In
+      </button>
+    </div>
+  </form>
+</template>
---
+++ components/common/RegisterForm.vue
@@ -0,0 +1,106 @@
+<template>
+  <form class="w-full space-y-3">
+    <div class="sm:flex sm:justify-between">
+      <div class="form-control">
+        <label class="label justify-start cursor-pointer">
+          <input
+            type="radio"
+            name="userType"
+            class="radio-xs checked:bg-accent mr-1"
+            checked
+          />
+          <span class="label-text">Entrepreneur</span>
+        </label>
+      </div>
+      <div class="form-control">
+        <label class="label justify-start cursor-pointer">
+          <input
+            type="radio"
+            name="userType"
+            class="radio-xs checked:bg-accent mr-1"
+          />
+          <span class="label-text">Investor</span>
+        </label>
+      </div>
+      <div class="form-control">
+        <label class="label justify-start cursor-pointer">
+          <input
+            type="radio"
+            name="userType"
+            class="radio-xs checked:bg-accent mr-1"
+          />
+          <span class="label-text">Partner/Organization</span>
+        </label>
+      </div>
+    </div>
+
+    <div class="border-b border-accent py-2">
+      <input
+        class="appearance-none bg-transparent w-full text-primary mr-3 px-2 leading-tight focus:outline-none"
+        type="text"
+        placeholder="Full Name"
+        aria-label="Full Name"
+      />
+    </div>
+
+    <div class="border-b border-accent py-2">
+      <select
+        class="select appearance-none bg-transparent w-full text-gray-400 mr-3 px-2 leading-tight focus:outline-none"
+      >
+        <option disabled selected text-primary>Cluster</option>
+        <option>Svelte</option>
+        <option>Vue</option>
+        <option>React</option>
+      </select>
+    </div>
+
+    <div class="border-b border-accent py-2">
+      <input
+        class="appearance-none bg-transparent w-full text-primary mr-3 px-2 leading-tight focus:outline-none"
+        type="text"
+        placeholder="Email/Mobile"
+        aria-label="Email/Mobile"
+      />
+    </div>
+
+    <div class="flex items-baseline border-b border-accent py-2">
+      <input
+        class="appearance-none bg-transparent w-full text-primary mr-3 px-2 leading-tight focus:outline-none"
+        type="password"
+        placeholder="Password"
+        aria-label="Password"
+      />
+      <label class="swap">
+        <input type="checkbox" />
+        <div class="swap-on"><i class="fa-solid fa-eye"></i></div>
+        <div class="swap-off">
+          <i class="fa-solid fa-eye-slash"></i>
+        </div>
+      </label>
+    </div>
+
+    <div class="flex items-baseline border-b border-accent py-2">
+      <input
+        class="appearance-none bg-transparent w-full text-primary mr-3 px-2 leading-tight focus:outline-none"
+        type="password"
+        placeholder="Confirm Password"
+        aria-label="Confirm Password"
+      />
+      <label class="swap">
+        <input type="checkbox" />
+        <div class="swap-on"><i class="fa-solid fa-eye"></i></div>
+        <div class="swap-off">
+          <i class="fa-solid fa-eye-slash"></i>
+        </div>
+      </label>
+    </div>
+
+    <div>
+      <button
+        class="mt-6 w-full bg-accent hover:bg-primary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Sign Up
+      </button>
+    </div>
+  </form>
+</template>
