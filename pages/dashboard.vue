b4d8bc84c540ea453ac3c312d24d5b1a053f4bc0
fariarahman15 <fariarahman.dev@gmail.com>
2023-08-22 15:18:10 +0600

    dashboard layout added

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,241 +1,164 @@
 <script setup lang="js">
 useHead({
-    title: "Login | Admin Panel ",
+    title: "Dashboard | Admin Panel ",
 });
 
 definePageMeta({
-    middleware: ["auth"],
+    // middleware: ["auth"],
     layout: "default",
 });
 const { $axios } = useNuxtApp()
-
-
-
-/*
-function getPosts() {
-  $axios.get('/user',{
-  })
-      .then(function (response) {
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
-}
-
-getPosts()
-*/
-
-
-
-
-
-
-
-/*
-const guardsToBeChecked = "dashboard_management_view,dashboard_management_create,dashboard_management_edit,dashboard_management_delete";
-const { checkGuards } = useCheckGuards(guardsToBeChecked);
-const {
-    hasViewPermission,
-    hasCreatePermission,
-    hasEditPermission,
-    hasDeletePermission
-} = checkGuards();
-
-*/
-
-
-
-
-
-
 </script>
 
 <template>
-
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard</h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard v1</li>
-                            </ol>
-                        </div><!-- /.col -->
-
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
+  <CommonContainer>
+    <div>
+      <h1 class="text-3xl font-bold">Dashboard</h1>
+      <hr class="my-2 border dark:border-gray-700" />
+      <div class="grid grid-cols-12 gap-2">
+        <div class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+          <div
+            class="p-2 rounded-lg bg-white dark:bg-gray-800 shadow flex justify-between gap-2 h-full"
+          >
+            <div class="flex flex-col justify-between">
+              <div class="text-gray-500 dark:text-gray-400 font-medium">
+                Today's Money
+              </div>
+              <div class="text-gray-700 dark:text-gray-200 font-bold text-lg">
+                $53,000
+                <span class="text-sm text-yellow-600">+55%</span>
+              </div>
             </div>
-            <!-- /.content-header -->
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <!-- Small boxes (Stat box) -->
-                    <div class="row">
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>150</h3>
-
-                                    <p>New Orders</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-bag"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                                    <p>Bounce Rate</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>44</h3>
-
-                                    <p>User Registrations</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-person-add"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>65</h3>
-
-                                    <p>Unique Visitors</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section>
-            <!-- /.content -->
-
-<!--    <div v-if="hasViewPermission">
-        <div class="content-wrapper">
-            &lt;!&ndash; Content Header (Page header) &ndash;&gt;
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard</h1>
-                        </div>&lt;!&ndash; /.col &ndash;&gt;
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard v1</li>
-                            </ol>
-                        </div>&lt;!&ndash; /.col &ndash;&gt;
-                        
-                    </div>&lt;!&ndash; /.row &ndash;&gt;
-                </div>&lt;!&ndash; /.container-fluid &ndash;&gt;
+            <div
+              class="bg-gradient-to-tl from-gray-600 to-gray-900 rounded-lg text-white p-2 flex items-center justify-center px-4"
+            >
+              <TwFeather type="dollar-sign"></TwFeather>
             </div>
-            &lt;!&ndash; /.content-header &ndash;&gt;
-
-            &lt;!&ndash; Main content &ndash;&gt;
-            <section class="content">
-                <div class="container-fluid">
-                    &lt;!&ndash; Small boxes (Stat box) &ndash;&gt;
-                    <div class="row">
-                        <div class="col-lg-3 col-6">
-                            &lt;!&ndash; small box &ndash;&gt;
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>150</h3>
-
-                                    <p>New Orders</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-bag"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        &lt;!&ndash; ./col &ndash;&gt;
-                        <div class="col-lg-3 col-6">
-                            &lt;!&ndash; small box &ndash;&gt;
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                                    <p>Bounce Rate</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        &lt;!&ndash; ./col &ndash;&gt;
-                        <div class="col-lg-3 col-6">
-                            &lt;!&ndash; small box &ndash;&gt;
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>44</h3>
-
-                                    <p>User Registrations</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-person-add"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        &lt;!&ndash; ./col &ndash;&gt;
-                        <div class="col-lg-3 col-6">
-                            &lt;!&ndash; small box &ndash;&gt;
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>65</h3>
-
-                                    <p>Unique Visitors</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        &lt;!&ndash; ./col &ndash;&gt;
-                    </div>
-                </div>&lt;!&ndash; /.container-fluid &ndash;&gt;
-            </section>
-            &lt;!&ndash; /.content &ndash;&gt;
+          </div>
         </div>
-    </div>-->
+        <div class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+          <div
+            class="p-2 rounded-lg bg-white dark:bg-gray-800 shadow flex justify-between gap-2 h-full"
+          >
+            <div class="flex flex-col justify-between">
+              <div class="text-gray-500 dark:text-gray-400 font-medium">
+                Today's Users
+              </div>
+              <div class="text-gray-700 dark:text-gray-200 font-bold text-lg">
+                2,300
+                <span class="text-sm text-yellow-600">+3%</span>
+              </div>
+            </div>
+            <div
+              class="bg-gradient-to-tl from-gray-600 to-gray-900 rounded-lg text-white p-2 flex items-center justify-center px-4"
+            >
+              <TwFeather type="aperture"></TwFeather>
+            </div>
+          </div>
+        </div>
+        <div class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+          <div
+            class="p-2 rounded-lg bg-white dark:bg-gray-800 shadow flex justify-between gap-2 h-full"
+          >
+            <div class="flex flex-col justify-between">
+              <div class="text-gray-500 dark:text-gray-400 font-medium">
+                New Clients
+              </div>
+              <div class="text-gray-700 dark:text-gray-200 font-bold text-lg">
+                +3,462
+                <span class="text-sm text-red-600">+2%</span>
+              </div>
+            </div>
+            <div
+              class="bg-gradient-to-tl from-gray-600 to-gray-900 rounded-lg text-white p-2 flex items-center justify-center px-4"
+            >
+              <TwFeather type="users"></TwFeather>
+            </div>
+          </div>
+        </div>
+        <div class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+          <div
+            class="p-2 rounded-lg bg-white dark:bg-gray-800 shadow flex justify-between gap-2 h-full"
+          >
+            <div class="flex flex-col justify-between">
+              <div class="text-gray-500 dark:text-gray-400 font-medium">
+                Sales
+              </div>
+              <div class="text-gray-700 dark:text-gray-200 font-bold text-lg">
+                $103,430
+                <span class="text-sm text-yellow-600">+5%</span>
+              </div>
+            </div>
+            <div
+              class="bg-gradient-to-tl from-gray-600 to-gray-900 rounded-lg text-white p-2 flex items-center justify-center px-4"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-12 gap-2 mt-2">
+        <div class="col-span-12 md:col-span-7">
+          <div
+            class="flex gap-2 rounded-lg p-4 shadow bg-white dark:bg-gray-800"
+          >
+            <div class="flex flex-col justify-between">
+              <div class="">
+                <div class="text-gray-500 dark:text-gray-400 font-medium">
+                  Built by developers
+                </div>
+                <div class="text-gray-700 dark:text-gray-200 font-bold text-xl">
+                  Nuxt 3 Admin Dashboard
+                </div>
+                <div class="">
+                  From colors, cards, typography to complex elements, you will
+                  find the full documentation.
+                </div>
+              </div>
+              <div class="">Read More ...</div>
+            </div>
+            <div
+              class="rounded-lg bg-gradient-to-tl from-gray-50 to-gray-200 dark:from-gray-700 flex items-center justify-center w-52 h-52"
+            >
+              <img
+                class="w-32 h-32 rounded object-cover"
+                src="/images/rocket.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div class="col-span-12 md:col-span-5">
+          <div
+            class="flex gap-2 bg-white dark:bg-gray-800 rounded-lg p-4 shadow"
+          >
+            <div class="flex flex-col justify-between">
+              <div class="">
+                <div class="text-gray-500 dark:text-gray-400 font-medium">
+                  Built by developers
+                </div>
+                <div class="text-gray-700 dark:text-gray-200 font-bold text-xl">
+                  Nuxt 3 Admin Dashboard
+                </div>
+                <div class="">
+                  From colors, cards, typography to complex elements, you will
+                  find the full documentation.
+                </div>
+              </div>
+              <div class="">Read More ...</div>
+            </div>
+            <div
+              class="rounded-lg bg-gradient-to-tl from-gray-50 to-gray-200 dark:from-gray-700 flex items-center justify-center w-52 h-52"
+            >
+              <img
+                class="w-32 h-32 rounded object-cover"
+                src="/images/rocket.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </CommonContainer>
 </template>
 
 <style scoped></style>
