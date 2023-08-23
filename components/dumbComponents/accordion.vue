cf18772e3e3e358d4166974cad73639abe9e853b
fariarahman15 <fariarahman.dev@gmail.com>
2023-08-23 17:03:55 +0600

    accordion component updated

--- components/dumbComponents/accordion.vue
+++ components/dumbComponents/accordion.vue
@@ -3,15 +3,47 @@
     <div class="join join-vertical w-full">
       <div
         class="collapse collapse-arrow join-item border border-base-300"
-        v-for="(item, index) in accordionItems"
+        v-for="(item, index) in items"
         :key="index"
       >
-        <input type="radio" name="my-accordion" />
-        <div class="collapse-title text-xl font-medium">{{ item.title }}</div>
-        <div class="collapse-content">
-          <p v-for="(menu, menuIndex) in item.menus" :key="menuIndex">
-            {{ menu }}
-          </p>
+        <input v-show="item.submenus" type="radio" name="accordion-menu" />
+        <div v-if="item.submenus" class="collapse-title text-xl font-medium">
+          {{ item.title }}
+        </div>
+        <div v-if="item.submenus" class="collapse-content">
+          <div
+            class="collapse collapse-arrow join-item"
+            v-for="(submenu, subIndex) in item.submenus"
+            :key="subIndex"
+          >
+            <input
+              v-show="submenu.items"
+              type="radio"
+              name="accordion-submenu"
+            />
+            <div
+              v-if="submenu.items"
+              class="collapse-title text-xl font-medium"
+            >
+              {{ submenu.menu }}
+            </div>
+            <div v-if="submenu.items" class="collapse-content">
+              <p
+                v-for="(subItem, subItemIndex) in submenu.items"
+                :key="subItemIndex"
+              >
+                {{ subItem }}
+              </p>
+            </div>
+
+            <div v-else class="p-4 text-xl font-medium">
+              {{ submenu.menu }}
+            </div>
+          </div>
+        </div>
+
+        <div v-else class="p-4 text-xl font-medium">
+          {{ item.title }}
         </div>
       </div>
     </div>
@@ -22,18 +54,17 @@
 export default {
   data() {
     return {
-      accordionItems: [
+      items: [
         {
-          title: "Title 1",
-          menus: ["Menu 1", "Menu 2"],
+          title: "Dashboard",
         },
         {
           title: "Title 2",
-          menus: ["Menu 3", "Menu 4"],
+          submenus: [{ menu: "Menu 3" }],
         },
         {
           title: "Title 3",
-          menus: ["Menu 5", "Menu 6"],
+          submenus: [{ menu: "Menu 5", items: ["Menu 5", "Menu 6"] }],
         },
       ],
     };
--- components/sideBar/sidebarDashboard.vue
+++ components/sideBar/sidebarDashboard.vue
@@ -6,29 +6,25 @@
         v-for="(item, index) in items"
         :key="index"
       >
-        <input type="radio" name="accordion-menu" />
+        <input v-show="item.submenus" type="radio" name="accordion-menu" />
         <div v-if="item.submenus" class="collapse-title text-xl font-medium">
           {{ item.title }}
         </div>
-
-        <div v-else class="p-4 text-xl font-medium">
-          {{ item.title }}
-        </div>
-
         <div v-if="item.submenus" class="collapse-content">
           <div
             class="collapse collapse-arrow join-item"
             v-for="(submenu, subIndex) in item.submenus"
             :key="subIndex"
           >
-            <input type="radio" name="accordion-submenu" />
+            <input
+              v-show="submenu.items"
+              type="radio"
+              name="accordion-submenu"
+            />
             <div
               v-if="submenu.items"
               class="collapse-title text-xl font-medium"
             >
-              {{ submenu.menu }}
-            </div>
-            <div v-else class="p-4 text-xl font-medium">
               {{ submenu.menu }}
             </div>
             <div v-if="submenu.items" class="collapse-content">
@@ -39,7 +35,15 @@
                 {{ subItem }}
               </p>
             </div>
+
+            <div v-else class="p-4 text-xl font-medium">
+              {{ submenu.menu }}
+            </div>
           </div>
+        </div>
+
+        <div v-else class="p-4 text-xl font-medium">
+          {{ item.title }}
         </div>
       </div>
     </div>
